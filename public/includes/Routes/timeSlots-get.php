423fa4d00db202713c86c1eb2e937f8be71e8d91
Steve Allen <steve1@rcconsulting.com>
2018-03-26 08:04:14 -0700

    Closes #6

---
+++ public/includes/Routes/timeSlots-get.php
@@ -0,0 +1,45 @@
+<?php
+use airmoi\FileMaker\Record;
+#use airmoi\FileMaker\Exception;
+use airmoi\FileMaker\FileMakerException;
+
+use rts_scheduler\V1\Entities\TimeSlot;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+$app -> get('/slots/{year}/{month}/{day}', function(Request $request, Response $response, array $args){
+
+	$date = strtotime($args['month'].'/'.$args['day'].'/'.$args['year']);
+
+	$db = connectToDB('BESI_SMART',['errorHandling' => 'exception']);
+	
+	try {
+		$q = $db -> newFindCommand('web_time_slots');
+
+		$q -> addFindCriterion('date', '=='.date('n/j/Y', $date));
+
+		$r = $q -> execute();
+
+		$records = $r -> getRecords();
+		$slots = [];
+		foreach($records as $record){
+			$slots[] = TimeSlot::generate(
+				TimeSlot::readFM($record)
+			);
+		}
+		// error_log('finished processing project load: ' . date('H:i:s:u', strtotime('now')));
+		return $response -> withJson(success([
+			'date' => date('Y-m-j', $date),
+			'slots' => $slots
+		]));
+	}
+	catch(Exception $e){
+		$code = $e->getCode();
+		$msg = $e->getMessage();
+		if($code == 401){
+			return $response -> withJson(success(['slots' => []]));
+		}
+		return $response -> withJson(error($code,$msg));
+	}
+});

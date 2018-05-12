4c929b701c62188299168ba62b2b35c49b80be34
Steve Allen <steve1@rcconsulting.com>
2018-05-11 22:51:48 -0700

    fixed typo in logger call

--- src/settings.php
+++ src/settings.php
@@ -23,7 +23,7 @@ return [
 		'logger' => [
 			'name' => 'slim-app',
 			'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
-			'level' => \Monolog\Logger::DEBUG,
+			'level' => Monolog\Logger::DEBUG,
 		],
 	],
 ];
